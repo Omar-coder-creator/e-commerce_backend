b48a759def29099bb5256bd1453b3630163996ac
omar <benghorbelomar5@gmail.com>
2025-03-28 15:07:17 +0100

    updating files

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -73,9 +73,12 @@ router.put('/:id/quantity/:prodId', async (req, res) => {
         if (!cart) return res.status(404).json({ message: 'cart not found' });
         const product = await Product.findById(req.params.prodId);
         if (!product) return res.status(404).json({ message: 'product not found' });
-
+        if(req.body.quantity < 1){
+            cart.items = cart.items.filter(item => String(item.productId) != String(product._id))
+            const newCart = await cart.save()
+            return res.status(200).json(newCart)
+        }
         cart.items = cart.items.map(item => String(item.productId) === String(product._id) ? { productId: product._id, quantity: req.body.quantity } : item)
-
         const newCart = await cart.save();
         res.status(200).json(newCart);
     } catch (err) {
